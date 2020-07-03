5cea778a52698075741f10ba4a1298a8f3f173f0
EmmanueleVilla <emmanuele.villa@finconsgroup.com>
2020-07-03 15:44:15 +0200

    update ios layout

--- ios/KaMPStarteriOS/SplashViewController.swift
+++ ios/KaMPStarteriOS/SplashViewController.swift
@@ -13,13 +13,29 @@ import SDWebImage
 class SplashViewController: UIViewController {
     
     let interactor: SplashInteractor = SplashInteractor()
-    let image = UIImage()
+    let image = UIImageView()
     let desc = UILabel()
     let manager = SDWebImageManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.cyan
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(image)
+        image.translatesAutoresizingMaskIntoConstraints = false
+        let topImage = image.topAnchor.constraint(equalTo: view.topAnchor)
+        let leftImage = image.leftAnchor.constraint(equalTo: view.leftAnchor)
+        let rightImage = image.rightAnchor.constraint(equalTo: view.rightAnchor)
+        let heightImage = image.heightAnchor.constraint(equalToConstant: 250)
+        NSLayoutConstraint.activate([topImage, leftImage, rightImage, heightImage])
+        
+        view.addSubview(desc)
+        desc.translatesAutoresizingMaskIntoConstraints = false
+        let topDesc = desc.topAnchor.constraint(equalTo: view.bottomAnchor)
+        let leftDesc = image.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 12)
+        let rightDesc = image.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 12)
+        NSLayoutConstraint.activate([topDesc, leftDesc, rightDesc])
+       
         interactor.subscribe(callback: { state in
             self.desc.text = state.splashState.apod.explanation
             self.manager.loadImage(with: URL(string: state.splashState.apod.url), options: SDWebImageOptions(), progress: { i, j, url in }, completed: { a,b,c,d,e,f  in })
