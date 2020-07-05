4f749a98b2784357709cf7e3c25b40967c47fd9c
EmmanueleVilla <emmanuele.villa@finconsgroup.com>
2020-07-05 18:56:51 +0200

    added server as external module because we can't have both android and jvmapplication in the same one

---
+++ server/src/main/kotlin/co/touchlab/kampstarter/Main.kt
@@ -0,0 +1,40 @@
+package co.touchlab.kampstarter
+
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.http.content.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.serialization.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import org.litote.kmongo.*
+import org.litote.kmongo.async.*
+import org.litote.kmongo.coroutine.*
+import org.litote.kmongo.async.getCollection
+import com.mongodb.ConnectionString
+
+fun main() {
+    embeddedServer(Netty, 9090) {
+        install(ContentNegotiation) {
+            json()
+        }
+        install(CORS) {
+            method(HttpMethod.Get)
+            method(HttpMethod.Post)
+            method(HttpMethod.Delete)
+            anyHost()
+        }
+        install(Compression) {
+            gzip()
+        }
+
+        routing {
+            get("/hello") {
+                call.respondText("Hello, this is a web server!")
+            }
+        }
+    }.start(wait = true)
+}
