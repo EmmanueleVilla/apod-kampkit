7b31b87091273d2995f71a6d689c1a572d04d4d4
EmmanueleVilla <emmanuele.villa@finconsgroup.com>
2020-07-08 12:02:31 +0200

    added youtube logo

--- app/src/main/java/com/shadowings/apodkmp/android/adapters/ApodAdapter.kt
+++ app/src/main/java/com/shadowings/apodkmp/android/adapters/ApodAdapter.kt
@@ -5,9 +5,11 @@ import android.graphics.Color
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.LinearLayout
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.recyclerview.widget.RecyclerView
+import co.touchlab.kampstarter.android.R
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
 import com.bumptech.glide.request.transition.DrawableCrossFadeFactory
@@ -42,21 +44,40 @@ class ApodViewHolder(itemView: ApodView) : RecyclerView.ViewHolder(itemView) {
         } else {
             (itemView as ApodView).image.setBackgroundResource(android.R.color.transparent)
         }
+        (itemView as ApodView).logo.visibility = if (apod.media_type == "video") View.VISIBLE else View.GONE
     }
 }
 
-class ApodView(context: Context) : LinearLayout(context) {
-    var image: AppCompatImageView = AppCompatImageView(context)
+class ApodView(context: Context) : ConstraintLayout(context) {
+    val image: AppCompatImageView = AppCompatImageView(context)
+    val logo: AppCompatImageView = AppCompatImageView(context)
+
     init {
         this.id = View.generateViewId()
         this.layoutParams = LayoutParams(Dimens.latestCardSize, Dimens.latestCardSize)
 
         image.id = View.generateViewId()
-        val layoutParams = MarginLayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
-        layoutParams.setMargins(Dimens.margin, Dimens.margin, Dimens.margin, Dimens.margin)
-        image.layoutParams = layoutParams
         image.scaleType = ImageView.ScaleType.CENTER_CROP
         image.setBackgroundColor(Color.GRAY)
         addView(image)
+
+        logo.id = View.generateViewId()
+        logo.scaleType = ImageView.ScaleType.CENTER_INSIDE
+        logo.setBackgroundResource(R.drawable.youtube_logo)
+        logo.visibility = View.GONE
+        addView(logo)
+
+        val set = ConstraintSet()
+        set.connect(image.id, ConstraintSet.START, this.id, ConstraintSet.START, Dimens.margin)
+        set.connect(image.id, ConstraintSet.END, this.id, ConstraintSet.END, Dimens.margin)
+        set.connect(image.id, ConstraintSet.TOP, this.id, ConstraintSet.TOP, Dimens.margin)
+        set.connect(image.id, ConstraintSet.BOTTOM, this.id, ConstraintSet.BOTTOM, Dimens.margin)
+
+        set.connect(logo.id, ConstraintSet.BOTTOM, image.id, ConstraintSet.BOTTOM, Dimens.margin)
+        set.connect(logo.id, ConstraintSet.START, image.id, ConstraintSet.START, Dimens.margin)
+        set.constrainHeight(logo.id, 125)
+        set.constrainWidth(logo.id, 125)
+
+        set.applyTo(this)
     }
 }
