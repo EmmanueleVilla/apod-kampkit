e49dfa35348d299d564a01fc487ee7231f853435
EmmanueleVilla <emmanuele.villa@finconsgroup.com>
2020-07-13 22:00:04 +0200

    update ios to use swiftUI

--- ios/ApodKMP/AppDelegate.swift
+++ ios/ApodKMP/AppDelegate.swift
@@ -8,18 +8,18 @@
 
 import UIKit
 import shared
+import SwiftUI
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let navigationController = UINavigationController();
-        window = UIWindow(frame: UIScreen.main.bounds);
-        window!.rootViewController = navigationController
-        window!.makeKeyAndVisible();
-        navigationController.setViewControllers([SplashViewController()], animated: false)
+        let window = UIWindow(frame: UIScreen.main.bounds);
+        window.rootViewController = UIHostingController(rootView: SplashView())
+        self.window = window
+        window.makeKeyAndVisible();
         return true
     }
 }
-
